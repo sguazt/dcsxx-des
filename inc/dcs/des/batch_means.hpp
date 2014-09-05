e8aba486700c99910be1d0e1067758c6355c551e
sguazt <marco.guazzone@gmail.com>
2014-09-05 10:54:20 +0200

    [core] Ditto

---
+++ inc/dcs/des/batch_means.hpp
@@ -0,0 +1,39 @@
+/**
+ * \file dcs/des/batch_means.hpp
+ *
+ * \brief Include all declarations needed by the batch means
+ * simulation method.
+ *
+ * \author Marco Guazzone (marco.guazzone@gmail.com)
+ *
+ * <hr/>
+ *
+ * Copyright 2014 Marco Guazzone (marco.guazzone@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef DCS_DES_BATCH_MEANS_HPP
+#define DCS_DES_BATCH_MEANS_HPP
+
+
+#include <dcs/des/batch_means/analyzable_statistic.hpp>
+#include <dcs/des/batch_means/dummy_batch_size_detector.hpp>
+#include <dcs/des/batch_means/engine.hpp>
+#include <dcs/des/batch_means/pawlikowski1990_batch_size_detector.hpp>
+#include <dcs/des/core.hpp>
+#include <dcs/des/spectral/pawlikowski1990_transient_detector.hpp>
+
+
+
+#endif // DCS_DES_BATCH_MEANS_HPP
