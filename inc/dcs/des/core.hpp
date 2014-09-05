87922154c8ecc6f598ea30f28e0555a0ceb0b1e2
sguazt <marco.guazzone@gmail.com>
2014-09-05 10:45:00 +0200

    [core] (new) Added convenient include files.

---
+++ inc/dcs/des/core.hpp
@@ -0,0 +1,54 @@
+/**
+ * \file dcs/des/core.hpp
+ *
+ * \brief Include all basic declarations needed by the simulation.
+ *
+ * \author Marco Guazzone (marco.guazzone@gmail.com)
+ *
+ * <hr/>
+ *
+ * Copyright 2014 Marco Guazzone (marco.guazzone@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef DCS_DES_CORE_HPP
+#define DCS_DES_CORE_HPP
+
+
+#include <dcs/des/analyzable_statistic_adaptor.hpp>
+#include <dcs/des/any_analyzable_statistic.hpp>
+#include <dcs/des/any_statistic.hpp>
+#include <dcs/des/base_analyzable_statistic.hpp>
+#include <dcs/des/base_statistic.hpp>
+#include <dcs/des/engine_context.hpp>
+#include <dcs/des/engine.hpp>
+#include <dcs/des/engine_traits.hpp>
+#include <dcs/des/entity.hpp>
+#include <dcs/des/event.hpp>
+#include <dcs/des/event_list.hpp>
+#include <dcs/des/event_source.hpp>
+#include <dcs/des/max_estimator.hpp>
+#include <dcs/des/mean_estimator.hpp>
+#include <dcs/des/min_estimator.hpp>
+#include <dcs/des/null_transient_detector.hpp>
+#include <dcs/des/output_analysis_categories.hpp>
+#include <dcs/des/output_analysis.hpp>
+#include <dcs/des/quantile_estimator.hpp>
+#include <dcs/des/statistic_adaptor.hpp>
+#include <dcs/des/statistic_categories.hpp>
+#include <dcs/des/utility.hpp>
+#include <dcs/des/weighted_mean_estimator.hpp>
+
+
+#endif // DCS_DES_CORE_HPP
---
+++ inc/dcs/des/replications.hpp
@@ -0,0 +1,42 @@
+/**
+ * \file dcs/des/replications.hpp
+ *
+ * \brief Include all declarations needed by the independent replications
+ * simulation.
+ *
+ * \author Marco Guazzone (marco.guazzone@gmail.com)
+ *
+ * <hr/>
+ *
+ * Copyright 2014 Marco Guazzone (marco.guazzone@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef DCS_DES_REPLICATIONS_HPP
+#define DCS_DES_REPLICATIONS_HPP
+
+
+#include <dcs/des/core.hpp>
+#include <dcs/des/replications.hpp>
+#include <dcs/des/replications/analyzable_statistic.hpp>
+#include <dcs/des/replications/banks2005_num_replications_detector.hpp>
+#include <dcs/des/replications/constant_num_replications_detector.hpp>
+#include <dcs/des/replications/dummy_num_replications_detector.hpp>
+#include <dcs/des/replications/dummy_replication_size_detector.hpp>
+#include <dcs/des/replications/engine.hpp>
+#include <dcs/des/replications/fixed_duration_replication_size_detector.hpp>
+#include <dcs/des/replications/fixed_num_obs_replication_size_detector.hpp>
+
+
+#endif // DCS_DES_REPLICATIONS_HPP
